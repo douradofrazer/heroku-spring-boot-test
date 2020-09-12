7a3421d7d8971600bb793c5d0602e1ba016638c6
Frazer Joe Dourado <douradofrazer@gmail.com>
2020-09-12 23:10:35 +0530

    First commit

---
+++ src/test/java/com/demo/herokuspringboot/HerokuspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.herokuspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
